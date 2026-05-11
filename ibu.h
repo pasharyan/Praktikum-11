36710b649b54ec09c7c87b3213cfc85cbfcc3984
pasharyan <pasharyan50@gmail.com>
2026-05-11 11:31:59 +0700

    masukkan class

--- ibu.h
+++ ibu.h
@@ -4,3 +4,23 @@
 #include <iostream>
 #include <string>
 #include "anak.h"
+
+using namespace std;
+
+class ibu
+{
+public:
+    string nama;
+    vector<anak *> daftar_anak;
+
+    ibu(string pNama) : nama(pNama)
+    {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu()
+    {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak *);
+    void cetakAnak();
+};
