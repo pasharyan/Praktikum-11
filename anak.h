e223569bb8c59929ce02aa2840940bab65174f00
pasharyan <pasharyan50@gmail.com>
2026-05-11 11:28:41 +0700

    masukkan include

--- anak.h
+++ anak.h
@@ -2,20 +2,3 @@
 #define ANAK_H
 #include <iostream>
 #include <string>
-
-using namespace std;
-
-class anak
-{
-public:
-    string nama;
-    anak(string pNama) : nama(pNama)
-    {
-        cout << "Anak \"" << nama << "\" ada\n";
-    }
-    ~anak()
-    {
-        cout << "Anak \"" << nama << "\" tidak ada\n";
-    }
-};
-#endif
